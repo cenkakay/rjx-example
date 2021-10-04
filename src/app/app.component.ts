d89a4f9c2f3198dd5a72440fc500e0dc8fc8ae37
cenkAkay <cenkakay@rotateknik.com.tr>
2021-10-04 15:54:16 +0300

    used subject instead of evetemitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { UserService } from "./user.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  constructor(private userService: UserService) {}
+  activatedSubscription: Subscription;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedSubscription = this.userService.activatedEmitter.subscribe(
+      (data) => {
+        this.userActivated = data;
+      }
+    );
+  }
+  ngOnDestroy() {
+    this.activatedSubscription.unsubscribe();
+  }
 }
